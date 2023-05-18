0ce88069e54982e3a5a9ae0f6012f0d7b7a58fc0
Leonardo carvalho <109566870+leonardocarMS@users.noreply.github.com>
2023-05-18 00:09:42 -0400

    Delete aplicação de controle remoto com gRPC directory

--- aplicação de controle remoto com gRPC/codigo.py
+++
@@ -1,5 +0,0 @@
-import controle_remoto_pb2
-import controle_remoto_pb2_grpc
-
-# Compila o arquivo .proto e gera o código Python correspondente
-python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. controle_remoto.proto
